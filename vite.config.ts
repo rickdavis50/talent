75f046bbe5ba3ca2e662fd948cb08601e16739c0
rickdavis50 <99823016+rickdavis50@users.noreply.github.com>
2026-01-10 15:35:02 -0800

    Fix Vite alias for GitHub Actions build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,7 +6,7 @@ export default defineConfig({
   base: '/talent/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': new URL('./src', import.meta.url).pathname,
     },
   },
 });
